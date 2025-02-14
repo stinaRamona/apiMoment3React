2007a891aedff3ab60887a1ca9ef38661a2d9382
Stina Persson <stpe1901@student.miun.se>
2025-02-14 13:49:21 +0100

    databaskoppling klar

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Mongoose = require("mongoose"); 
+require("dotenv").config(); 
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost', 
+        routes: {
+            cors: {
+                origin: ['*'], //Cors ok
+                credentials: true // cookie ok
+            }
+        }
+    });
+
+    //koppla till databasen 
+    Mongoose.connect(process.env.DATABASE).then(() => {
+        console.log("Ansluten till MongoDB"); 
+    }).catch((error) => {
+        console.log("Fel vid anslutning:" + error); 
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
