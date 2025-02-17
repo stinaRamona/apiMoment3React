9c37604b922eaa803cf1da47260a99ad35d9acd1
Stina Persson <stpe1901@student.miun.se>
2025-02-17 10:44:49 +0100

    gjort rutt för att lägga till användare

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -8,14 +8,32 @@ const authRouteArr = [
     //För att lägga till användare 
     {
         method: "POST", 
-        path: "/adduser", 
-
+        path: "/adduser",
+        options: {
+            validate: {
+                payload: Joi.object({
+                    user_name: Joi.string.min(3).required(), 
+                    email: Joi.string.email().required(), 
+                    password: Joi.string().min(5).required()
+                })
+            }
+        },
+        handler: async (request, h) => {
+            const user = new Admin(request.payload);
+            return await user.save();
+        } 
     }, 
+
+    //För att logga in 
+    {
+        method: "POST", 
+        path: "login/auth", 
+    },
 
     //För skyddad rutt
     {
         method: "GET", 
-        path: "/protected"
+        path: "/protected",
     }, 
 
 
