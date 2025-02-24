554f5f96067ed4f37b2fafbe70b8cdbc856e7101
Stina Persson <stpe1901@student.miun.se>
2025-02-24 14:12:52 +0100

    test av uppdatering för postanrop

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const init = async () => {
         routes: {
             cors: {
                 origin: ['*'], //Cors ok
-                credentials: true // cookie ok
+                //credentials: true // cookie ok
             }
         }
     });
